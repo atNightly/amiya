f03681a2628b106289196cb35584b2a81754d3a7
GitHub Action <runner@fv-az76.dazsf0pd55uejpfxwur3w1yv5c.cx.internal.cloudapp.net>
2020-07-22 13:19:00 +0000

    ghp-upload script

---
+++ master/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["amiya"] = [{"text":"impl&lt;Ex&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"amiya/middleware/struct.MethodRouter.html\" title=\"struct amiya::middleware::MethodRouter\">MethodRouter</a>&lt;Ex&gt;","synthetic":false,"types":["amiya::middleware::router::method::MethodRouter"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ master/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["amiya"] = {"name":"","dirs":[{"name":"middleware","dirs":[{"name":"router","files":["method.rs","mod.rs","set_which.rs","setter.rs"]}],"files":["mod.rs"]}],"files":["context.rs","lib.rs"]};
+createSourceSidebar();
